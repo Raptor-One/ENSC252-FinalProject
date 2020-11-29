d2b424ec5ff32a99483c69917d9c9a4671e0c830
Aru <aru_bhola@yahoo.in>
2020-11-28 21:54:10 -0800

    PE work

---
+++ STPU/LoadRegister.vhd
@@ -0,0 +1,27 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE IEEE.NUMERIC_STD.all;
+
+ENTITY LoadRegister IS
+PORT( clock, ld, reset : IN STD_LOGIC;
+		D : IN UNSIGNED(7 DOWNTO 0);
+		Q : OUT UNSIGNED(7 DOWNTO 0));
+END LoadRegister;
+
+ARCHITECTURE structure of LoadRegister IS
+	SIGNAL temp : UNSIGNED(7 DOWNTO 0);
+BEGIN 
+
+	PROCESS(clock, reset)
+	BEGIN
+	IF(reset = '1') then
+		temp <= (others=>'0'); 
+	ELSIF (rising_edge(clock) AND ld = '1') then
+		temp <= D;
+	END IF;
+	
+	END PROCESS;
+	
+	Q <= temp;
+	
+END structure;
--- STPU/PE.vhd
+++ STPU/PE.vhd
@@ -3,8 +3,18 @@ USE IEEE.STD_LOGIC_1164.all;
 USE IEEE.NUMERIC_STD.all;
 
 ENTITY PE IS
-PORT( clock, reset, hard_reset, ld, ld_w : IN STD_LOGIC;
+PORT (clock, reset, hard_reset, ld, ld_w : IN STD_LOGIC;
 		a_in, w_in, part_in : IN UNSIGNED(7 DOWNTO 0);
 		partial_sum, a_out : OUT UNSIGNED(7 DOWNTO 0));
 END PE;
 
+ARCHITECTURE STRUCTURE OF PE IS 
+
+COMPONENT LoadRegister IS
+	PORT (clock, ld, reset : IN STD_LOGIC;
+			D : IN UNSIGNED(7 DOWNTO 0);
+			Q : OUT UNSIGNED(7 DOWNTO 0));
+END LoadRegister;
+
+COMPONENT MAC IS 
+	PORT (
